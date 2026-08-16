d9ddeae33e321ed99c625275ec555139dc300555
Manvendra Singh <manvendrasingh2260@gmail.com>
2026-08-16 15:24:49 +0530

    ....

--- Basics/airthmetic.java
+++ Basics/airthmetic.java
@@ -4,6 +4,7 @@ public class airthmetic {
     public static void main(String[] args) {
         int x = 5;
         double y = 2;
+//        jhhhihidhiehi
         System.out.println(x/y);
         // int/int = int;
         // double/int = double; OR int/double = double
