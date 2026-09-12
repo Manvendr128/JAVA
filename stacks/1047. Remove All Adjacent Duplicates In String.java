3b2d84cc9b6b42ecc3f70be1c6ec09c07c60ef20
Manvendra Singh <manvendrasingh2260@gmail.com>
2026-09-12 15:51:25 +0530

    Add solution for removing adjacent duplicates in string
    Implemented a solution to remove adjacent duplicates from a string using a stack.

---
+++ stacks/1047. Remove All Adjacent Duplicates In String.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+import java.util.ArrayList;
+
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> st = new Stack<>();
+        st.push(s.charAt(0));
+        for(int i = 1;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(st.size()>0 && st.peek() == ch){
+                st.pop();
+            }
+            else{
+                st.push(ch);
+            }
+        }
+        ArrayList<Character> arr = new ArrayList<>();
+        while(st.size()>0){
+            arr.add(st.peek());
+            st.pop();
+        }
+        StringBuilder ans = new StringBuilder();
+        for(int i = arr.size()-1;i>=0;i--){
+            ans.append(arr.get(i));
+        }
+
+        return ans.toString();
+    }
+}
